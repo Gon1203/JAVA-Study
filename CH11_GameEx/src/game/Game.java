a4078563fcd9e795663e6f410595dcf8a2b06862
cookieGon <jygon931203@gmail.com>
2021-11-26 14:42:50 +0900

    가위바위보

--- CH11_GameEx/src/game/Game.java
+++ CH11_GameEx/src/game/Game.java
@@ -1,6 +1,7 @@
 package game;
 
 import java.util.Random;
+import java.util.Scanner;
 
 import game.objects.GameObject;
 import game.objects.Paper;
@@ -11,12 +12,30 @@ public class Game {
 
 	GameObject[] objects = { new Scisors(), new Rock(), new Paper() };
 	Random random = new Random();
+	Scanner sc = new Scanner(System.in);
 
 	public void run() {
 		System.out.println("게임 시작 ....");
+		int wins = 0;
+		for (int i = 0; i < 3; i++) {
 
-		int num = random.nextInt(objects.length);
-		GameObject ob = objects[num];
-		System.out.println(ob); // GameObject에서 toString을 오버라이드 했기 때문에 주소 대신 ob(.toString)이 출
+			System.out.print("가위(0), 바위(1), 보(2)를 숫자로 선택 >>");
+			GameObject ob1 = objects[sc.nextInt()];
+			GameObject ob2 = objects[random.nextInt(objects.length)];
+			System.out.println("당신의 선택은 : " + ob1.getName()); // GameObject에서 toString을 오버라이드 했기 때문에 주소 대신
+																// ob(.toString)이 출력
+			System.out.println("컴퓨터의 선택은 : " + ob2.getName());
+
+			int score = ob1.compareTo(ob2);
+			wins += score;
+		}
+		if (wins > 0) {
+			System.out.println("당신의 승리!");
+		} else if (wins < 0) {
+			System.out.println("당신이 졌습니다.");
+		} else {
+			System.out.println("비겼습니다");
+		}
+		sc.close();
 	}
 }
--- CH11_GameEx/src/game/objects/GameObject.java
+++ CH11_GameEx/src/game/objects/GameObject.java
@@ -3,7 +3,17 @@ package game.objects;
 public class GameObject {
 	private int id;
 	private String name;
-
+	private int[][] comparison = { 
+					//가위, 바위, 	보 
+/*가위*/			{ 0,	-1,		 1 }, 
+/*바위*/			{ 1,	 0,	 	-1 }, 
+/*보*/				{ -1,	 1,		 0 }
+	};
+	
+	// 상대의 가위 바위 보와 비교해서 결과 리턴
+	public int compareTo(GameObject ob) {
+		return comparison[id][ob.id];
+	}
 	protected int getId() { // 아이디 수정은 상속한 클래스에서만 사용
 		return id;
 	}
@@ -20,7 +30,7 @@ public class GameObject {
 		this.name = name;
 	}
 
-	@Override // 원래 object의 toString 메소드는 주소값을 나타내는데 메소드 오버라이드
+	@Override // 원래 object의 toString 메소드는 주소값을 나타내는데 메소드를 오버라이드해서 데이터를 출력
 	public String toString() {
 		return "GameObject [id=" + id + ", name=" + name + "]";
 	}
